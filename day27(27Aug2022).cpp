35a14aeac14f320214ff608cade3f7aac85abf61
YASH PASSI <98830689+kingpassi@users.noreply.github.com>
2022-08-30 01:01:48 +0530

    Add files via upload

---
+++ day27(27Aug2022).cpp
@@ -0,0 +1,101 @@
+/******************************************************************************
+363. Max Sum of Rectangle No Larger Than K
+Given an m x n matrix matrix and an integer k, return the max sum of a
+rectangle in the matrix such that its sum is no larger than k.
+
+It is guaranteed that there will be a rectangle with a sum no larger than k.
+
+ 
+
+Example 1:
+
+
+Input: matrix = [[1,0,1],[0,-2,3]], k = 2
+Output: 2
+Explanation: Because the sum of the blue rectangle [[0, 1], [-2, 3]] is 2,
+and 2 is the max number no larger than k (k = 2).
+Example 2:
+
+Input: matrix = [[2,2,-1]], k = 3
+Output: 3
+*******************************************************************************/
+class Solution {
+public:
+	// TIME COMPLEXITY:- O(M^2 NLOG(N))
+	// SPACE COMPLEXITY:- O(N)
+    int maxSumSubmatrix(vector<vector<int>>& matrix, int k) {
+    //     int n = matrix.size();
+    //     int m = matrix[0].size();
+    //     int ans = INT_MIN;
+    //     // prefix sum of every row
+    //     for(int i=0;i<n;i++){
+    //         for(int j=1;j<m;j++)
+    //             matrix[i][j]+=matrix[i][j-1];
+    //     }
+    //     // try every possible width of subarray
+    //     for(int start=0;start<m;start++){
+    //         for(int end=start;end<m;end++){
+    //             set<int> s = {0};
+    //             int pref_sum = 0;
+    //             // for current width of rectangle
+    //             for(int i=0;i<n;i++){
+    //                 int sum = matrix[i][end];
+    //                 if(start>0)
+    //                     sum-=matrix[i][start-1];
+    //                 pref_sum+=sum;
+    //                 auto itr = s.lower_bound(pref_sum-k);
+    //                 if(itr!=s.end())
+    //                     ans = max(ans,pref_sum-(*itr));
+    //                 s.insert(pref_sum);
+    //             }
+    //         }
+    //     }
+    //     return ans;
+    
+        int ans = INT_MIN;
+        
+        int R = matrix.size();
+        int C = matrix[0].size();
+        int arr[R];
+        
+        for(int l = 0; l < C; ++l) {
+            memset(arr, 0, sizeof(arr));
+            
+            for(int r = l; r < C; ++r) 
+            {
+                for(int up = 0; up < R; ++up)
+                {
+                    arr[up] += matrix[up][r]; 
+                }
+                int sum = 0, g = INT_MIN;
+                for(int up = 0; up < R; ++up) 
+                {
+                    sum = max(sum + arr[up], arr[up]);
+                    g = max(g, sum);
+                    if(g == k) 
+                        return k;
+                }
+                if(g < k) 
+                {
+                    ans = max(ans, g);
+                } 
+                else 
+                {
+                    for(int up1 = 0; up1 < R; ++up1) 
+                    {
+                        sum = 0;
+                        for(int up2 = up1; up2 < R; ++up2) 
+                        {
+                            sum += arr[up2];
+                            if(sum <= k) 
+                                ans = max(ans, sum);
+                        }
+                        
+                        if(ans == k) return k;
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+};
